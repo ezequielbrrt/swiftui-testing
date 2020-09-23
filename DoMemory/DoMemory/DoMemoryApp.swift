002fc3cac8cd1d4cb92e522d230d5fbe4f28c91f
Ezequiel Barreto <>
2020-09-23 12:45:27 -0500

    main files

---
+++ DoMemory/DoMemory/DoMemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoMemoryApp.swift
+//  DoMemory
+//
+//  Created by Ezequiel Barreto on 23/09/20.
+//
+
+import SwiftUI
+
+@main
+struct DoMemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
